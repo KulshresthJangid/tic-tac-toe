a60207fa923670fbbd87deab72aa709c67c6e43f
Kulshresth Jangid <kulshresth.jangid@rampwin.com>
2024-12-21 13:05:32 +0530

    added game bhoard and player chagne name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Player from './components/Player';
+import Gameboard from './components/Gameboard';
+
 function App() {
-    return <h1></h1>;
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player name={'Player1'} symbol={'X'} />
+                    <Player name={'Player2'} symbol={'O'} />
+                </ol>
+                <Gameboard />
+            </div>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function Player({ name, symbol }) {
+    const [isEditing, setPlayerEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(name);
+
+    const handleEditing = () => setPlayerEditing((isEditing) => !isEditing);
+
+    const handleChangeName = (event) => {
+        setPlayerName(event.target.value);
+    };
+
+    let player;
+    if (isEditing) {
+        player = (
+            <input
+                type="text"
+                value={playerName}
+                required
+                onChange={handleChangeName}
+            />
+        );
+    } else {
+        player = <span className="player-name">{playerName}</span>;
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {player}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditing}>Edit</button>
+        </li>
+    );
+}
